a7dd3ed0915828c11b0dc6534d029e587f523751
Roman Kuznetsov <kuzroman@list.ru>
2021-03-17 22:53:09 +0300

    Revert "refactor file name for UI"
    This reverts commit 28fd9788ddb2d6b1ed4a80da844ab90a8f39409f.

---
+++ src/components/UI/_LoaderLine.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ui_loader-line">
+    <div class="loader" :style="{ width }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UI_LoaderLine',
+  props: {
+    percent: { type: Number, default: 50 },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    width() {
+      return `${this.percent}%`
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import './_props.scss';
+
+.ui_loader-line {
+  width: 18em;
+  height: 1.6em;
+  background: $bg-2;
+
+  .loader {
+    height: 100%;
+    background: $bg-10;
+  }
+}
+</style>
